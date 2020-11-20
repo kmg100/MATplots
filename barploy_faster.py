b010e0ea6ac986f71820eb9acdd58c219c952837
kmg100 <51814761+kmg100@users.noreply.github.com>
2020-11-20 09:40:05 +0200

    Update barploy_faster.py
    pievienota krasu maina atskiriba no lieluma

--- barploy_faster.py
+++ barploy_faster.py
@@ -2,19 +2,31 @@ import matplotlib.pyplot as plt
 from matplotlib import animation
 import random
 import time
-
-
 fig=plt.figure()
-
+y = 50
+global col
+col = []
 x=range(1,6)
-barcollection = plt.bar(x,50)
+barcollection = plt.bar(x,y, color = col)
 t_start = time.time()
 def animate(i):
     for i in range(len(barcollection)):
-    	barcollection[i].set_height(random.randint(0,50))
+        val = random.randint(0,50)
+        barcollection[i].set_height(val)
+        print(val)
+        if val < 10:
+            print("blue")
+            barcollection[i].set_color("blue")
+        elif val >= 25:
+            print("green")
+            barcollection[i].set_color("green")
+        else:
+            print("red")
+            barcollection[i].set_color("red")
 	#fps = (( i + 1 ) / (time.time() - t_start))
 	#print(fps)
-anim=animation.FuncAnimation(fig,animate,blit=False,interval=1)
+    #barcollection = plt.bar(x,y,color = col)
+anim=animation.FuncAnimation(fig,animate,blit=False,interval=1000)
 
 
 plt.show()
